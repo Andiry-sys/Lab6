cf784b9e2ec15a95850b769980faced594c0a57f
Andriy-sys <marchycandriy@gmail.com>
2022-01-12 20:55:02 +0200

    HTML and Js

---
+++ Task1/app.js
@@ -0,0 +1,11 @@
+let rectangle = { x1: 1, y1: 2, x2: 10, y2: 20 }
+
+function GetPointes(rect) {
+    console.log(rect.x1);
+    console.log(rect.y1);
+    console.log(rect.x2);
+    console.log(rect.y2);
+}
+
+
+GetPointes(rectangle)
